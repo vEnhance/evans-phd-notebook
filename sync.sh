f0ffd87533c47d6bf80928fff391e853fd9bd456
Evan Chen <evan@evanchen.cc>
2023-09-26 00:00:41 -0400

    Add latexmk to sync command
    No sense syncing a stale copy

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+latexmk
 gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
 gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
