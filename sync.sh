e0a5050c51aab0a549d3fc3ad4f43b9c5fbf0514
Evan Chen <evan@evanchen.cc>
2024-09-04 20:29:14 -0400

    Sync script for thesis draft

--- sync.sh
+++ sync.sh
@@ -3,5 +3,8 @@
 set -euxo pipefail
 
 latexmk main
+latexmk -cd thesis/thesis.tex
 gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
+gsutil cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf
 gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
+gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/thesis-draft.pdf
