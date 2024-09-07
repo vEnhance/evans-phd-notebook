ff027eb0703c3f785ad88f0e9d706a779d5f1e04
Evan Chen <evan@evanchen.cc>
2024-09-07 11:36:38 -0400

    chore: Ignore SyntaxWarning in gsutil

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 latexmk main
 latexmk -cd thesis/thesis.tex
-gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
-gsutil cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf
-gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
-gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/thesis-draft.pdf
+PYTHONWARNINGS="ignore" gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
+PYTHONWARNINGS="ignore" gsutil cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf
+PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
+PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/thesis-draft.pdf
