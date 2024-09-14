a4f116772f44643b07dd89bf06e98acf6df8a1b6
Evan Chen <evan@evanchen.cc>
2024-09-14 19:37:14 -0400

    ci: Pause updating main notebook for now

--- sync.sh
+++ sync.sh
@@ -2,9 +2,9 @@
 
 set -euxo pipefail
 
-latexmk main
+# latexmk main
 latexmk -cd thesis/thesis.tex
-PYTHONWARNINGS="ignore" gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
+# PYTHONWARNINGS="ignore" gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
 PYTHONWARNINGS="ignore" gsutil cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf
-PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
+# PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
 PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/thesis-draft.pdf
