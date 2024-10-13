c81c3658884976743db15a67c63d1c081e2548ef
Evan Chen <evan@evanchen.cc>
2024-10-12 20:26:49 -0400

    ci: Specify -pdf flag for thesis.tex

--- sync.sh
+++ sync.sh
@@ -3,7 +3,8 @@
 set -euxo pipefail
 
 # latexmk main
-latexmk -cd thesis/thesis.tex
+latexmk -pdf -cd thesis/thesis.tex
+
 # PYTHONWARNINGS="ignore" gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
 PYTHONWARNINGS="ignore" gsutil cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf
 # PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
