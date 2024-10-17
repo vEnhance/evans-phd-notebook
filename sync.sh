984c91a79c36fa0a7f0f5257c1dcfd155dcd36e8
Evan Chen <evan@evanchen.cc>
2024-10-17 15:17:45 -0400

    chore: Update gsutil to gcloud storage

--- sync.sh
+++ sync.sh
@@ -5,7 +5,4 @@ set -euxo pipefail
 # latexmk main
 latexmk -pdf -cd thesis/thesis.tex
 
-# PYTHONWARNINGS="ignore" gsutil cp main.pdf gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
-PYTHONWARNINGS="ignore" gsutil cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf
-# PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/evans-phd-notebook.pdf
-PYTHONWARNINGS="ignore" gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/thesis-draft.pdf
+gcloud storage cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf --cache-control="private,max-age=0"
