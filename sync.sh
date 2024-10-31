754fdc6a12f8fe9cfe991f187fac818a1787a81b
Evan Chen <evan@evanchen.cc>
2024-10-31 01:58:32 -0400

    chore: Upload slides too

--- sync.sh
+++ sync.sh
@@ -4,5 +4,7 @@ set -euxo pipefail
 
 # latexmk main
 latexmk -pdf -cd thesis/thesis.tex
+latexmk -cd slides/slides.tex
 
 gcloud storage cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf --cache-control="private,max-age=0"
+gcloud storage cp slides/slides.pdf gs://web.evanchen.cc/upload/thesis-slides.pdf --cache-control="private,max-age=0"
