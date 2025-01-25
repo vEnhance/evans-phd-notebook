024164403c537a41c19eaa61b2f8e870133124fc
Evan Chen <evan@evanchen.cc>
2025-01-25 13:30:41 -0500

    build: update links to thesis stuff

--- sync.sh
+++ sync.sh
@@ -2,9 +2,10 @@
 
 set -euxo pipefail
 
-# latexmk main
+latexmk main
 latexmk -pdf -cd thesis/thesis.tex
 latexmk -cd slides/slides.tex
 
-gcloud storage cp thesis/thesis.pdf gs://web.evanchen.cc/upload/thesis-draft.pdf --cache-control="private,max-age=0"
-gcloud storage cp slides/slides.pdf gs://web.evanchen.cc/upload/thesis-slides.pdf --cache-control="private,max-age=0"
+gcloud storage cp chen-evanchen-phd-math-2025-thesis.pdf gs://web.evanchen.cc/textbooks/chen-evanchen-phd-math-2025-thesis.pdf --cache-control="private,max-age=0"
+gcloud storage cp slides/slides.pdf gs://web.evanchen.cc/textbooks/thesis-slides.pdf --cache-control="private,max-age=0"
+gcloud storage cp main.pdf gs://web.evanchen.cc/textbooks/phd-notebook.pdf --cache-control="private,max-age=0"
