5fccc8d5f3233a833150664f1f02f9860b7a9177
Evan Chen <evan@evanchen.cc>
2025-02-09 17:45:48 -0500

    chore: Cleaner script for getting arXiv version

---
+++ arxiv/clean.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+thesis_mode = False
+
+with open("targets") as f:
+    for filename in f:
+        filename = filename.strip()
+        target = Path("../thesis/") / filename
+        s = ""
+        with open(target) as target_file:
+            for line in target_file:
+                line = line.replace(r"\subsubsubsection", r"\paragraph")
+                if line.strip() == r"\ifthesis":
+                    thesis_mode = True
+                    continue
+                elif line.strip() == r"\ifpaper":
+                    continue
+                elif line.strip() == r"\fi":
+                    thesis_mode = False
+                    continue
+                if thesis_mode is False:
+                    s += line
+        with open(filename, "w") as out_file:
+            print(s.strip(), file=out_file)
